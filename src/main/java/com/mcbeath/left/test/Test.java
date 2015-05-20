f51296a8bd3c64767de5c21914f4608d0314aec6
mcbeath <mcbeathtao@gmail.com>
2015-05-20 17:06:57 +0800

    update Test.java

--- src/main/java/com/mcbeath/left/test/Test.java
+++ src/main/java/com/mcbeath/left/test/Test.java
@@ -1,7 +1,10 @@
 package com.mcbeath.left.test;
 
 public class Test {
-     public static void main(String[] args) {
-		System.out.println("Test.main()");
+    public static void main(String[] args) {
+		print("Test.main()");
 	}
+    public static void print(String b){
+    	System.out.println(b);
+    }
 }
